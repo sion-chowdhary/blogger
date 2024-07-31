67f29f997f28eb0c432485e9687c1794581a1420
SION CHOWDHARY <94757639+sion-chowdhary@users.noreply.github.com>
2024-08-01 05:11:51 +0530

    Add existing project files to Git

---
+++ app/page.js
@@ -0,0 +1,17 @@
+'use client'
+import BlogList from "@/Components/BlogList";
+import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Home() {
+  return (
+     <>
+        <ToastContainer theme="dark"/>
+        <Header />
+        <BlogList />
+        <Footer />
+     </>
+  );
+}
